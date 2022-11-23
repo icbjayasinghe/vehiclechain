72148f7025df8b8899ee45f487ded8e78a5138bb
Isuru <icbjayasinghe@gmail.com>
2022-11-23 22:37:15 +0530

    Added swagger specification

---
+++ vehicle-mgt-module/src/main/java/com/skyhigh/vehiclechain/VehiclechainApplication.java
@@ -0,0 +1,13 @@
+package com.skyhigh.vehiclechain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclechainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclechainApplication.class, args);
+    }
+
+}
